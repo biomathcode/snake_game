3cea6ee12c8b4483ba9123ece1b144c607838f4e
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-03-09 22:56:46 +0530

    initial grid

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,21 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[wasm_bindgen]
+pub struct World {
+    pub width: usize,
+}
+
+#[wasm_bindgen]
+impl World {
+    pub fn new() -> World {
+        World { width: 16 }
+    }
+
+    pub fn get_w(&self) -> usize {
+        self.width
+    }
+}
+
 // use extern to get function in the webassembly
 // wasm-pack build --target web
--- view/index.js
+++ view/index.js
@@ -1,7 +1,33 @@
-import init, { greet } from "snake_game";
+import init, { World } from "snake_game";
 
 // load webassembly than run the function
 
 init().then((_) => {
-  greet("Pratik is great ");
+  const CELL_SIZE = 10;
+  const world = World.new();
+  const worldWidth = world.get_w();
+
+  const canvas = document.getElementById("canvas");
+
+  const ctx = canvas.getContext("2d");
+  canvas.width = world.get_w() * CELL_SIZE;
+  canvas.height = world.get_w() * CELL_SIZE;
+
+  function drawWorld() {
+    ctx.beginPath();
+
+    for (let x = 0; x < worldWidth + 1; x++) {
+      ctx.moveTo(CELL_SIZE * x, 0);
+      ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE);
+    }
+    for (let y = 0; y < worldWidth + 1; y++) {
+      ctx.moveTo(0, CELL_SIZE * y);
+      ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y);
+    }
+    ctx.stroke();
+  }
+
+  drawWorld();
+
+  console.log(ctx);
 });
