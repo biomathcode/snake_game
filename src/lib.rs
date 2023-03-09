3ffa7e5afa01bf641f1658555ea8ef3cf08105e0
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-03-09 18:10:50 +0530

    snake game

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+use wasm_bindgen::prelude::*;
+
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// use extern to get function in the webassembly
+// wasm-pack build --target web
