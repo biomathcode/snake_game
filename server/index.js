1705638d2f1c36d137040077309c70bcd92c635f
Pratik Sharma <sharma.pratik2016@gmail.com>
2023-03-11 01:08:45 +0530

    🚀 deployment

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const compression = require("compression");
+
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const public = path.join(__dirname, "..", "view", "public");
+
+app.use(compression());
+
+app.use(express.static(public));
+
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root: public });
+});
+
+app.listen(port, () => {
+  console.log("server is running");
+});
